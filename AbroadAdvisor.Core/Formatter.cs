372100cf12f7bcc13e6f1aa5c0a609497c100492
Andrei Nicholson <contact@andreinicholson.com>
2014-02-22 21:37:50 -0500

    Add formatted phone number to student list

---
+++ AbroadAdvisor.Core/Formatter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Bennett.AbroadAdvisor.Core
+{
+    /// <summary>
+    /// Various formatting routines.
+    /// </summary>
+    public static class Formatter
+    {
+        /// <summary>
+        /// Format a phone number into parts.
+        /// </summary>
+        /// <remarks>
+        /// Phone number is expected to be 10 digits -- area code, prefix, and
+        /// line number.
+        /// </remarks>
+        /// <param name="number">10-digit phone number.</param>
+        /// <returns>Formatted number.</returns>
+        public static string PhoneNumber(string number)
+        {
+            if (String.IsNullOrWhiteSpace(number) || number.Length != 10)
+            {
+                return String.Empty;
+            }
+            return String.Format("{0:(###) ###-####}", Double.Parse(number));
+        }
+    }
+}
