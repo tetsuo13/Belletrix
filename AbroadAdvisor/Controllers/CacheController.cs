c1e19bd2d0e7074b51eed124af56ecd29414ef17
Andrei Nicholson <contact@andreinicholson.com>
2014-03-19 23:08:40 -0400

    API for building cache

---
+++ AbroadAdvisor/Controllers/CacheController.cs
@@ -0,0 +1,28 @@
+﻿using Bennett.AbroadAdvisor.Models;
+using System.Collections.Generic;
+using System.Web.Http;
+
+namespace Bennett.AbroadAdvisor.Controllers
+{
+    public class CacheController : ApiController
+    {
+        // GET api/cache
+        public IEnumerable<string> Get()
+        {
+            CountryModel.GetCountries();
+            LanguageModel.GetLanguages();
+            StudentClassificationModel.GetClassifications();
+            MajorsModel.GetMajors();
+            MinorsModel.GetMinors();
+            CountryModel.GetRegions();
+            StudentStudyAbroadWishlistModel.GetPeriods();
+            ProgramModel.GetPrograms();
+            ProgramTypeModel.GetProgramTypes();
+
+            StudentModel.GetStudents(null);
+            StudyAbroadModel.GetAll();
+
+            return new string[] { "foo" };
+        }
+    }
+}
