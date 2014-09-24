8232e0134f6a09fcf867b0ae7fe406b92a7e689f
Andrei Nicholson <contact@andreinicholson.com>
2014-09-23 22:30:05 -0400

    Internal function to delete a student

--- Schema/schema.sql
+++ Schema/schema.sql
@@ -728,3 +728,23 @@ FOREIGN KEY (program_type_id) REFERENCES program_types (id)
 GRANT ALL PRIVILEGES ON study_abroad_program_types TO neoanime_abroadadvisor;
 
 
+CREATE OR REPLACE FUNCTION delete_student(INT) RETURNS VOID AS $delete_student$
+    DECLARE
+        p_student_id ALIAS FOR $1;
+    BEGIN
+        RAISE NOTICE 'Deleting student ID %', p_student_id;
+
+        IF NOT EXISTS (SELECT 1 FROM students WHERE id = p_student_id) THEN
+            RAISE EXCEPTION 'Student ID % not found', p_student_id;
+        END IF;
+
+        DELETE FROM event_log WHERE student_id = p_student_id;
+        DELETE FROM matriculation WHERE student_id = p_student_id;
+        DELETE FROM student_desired_languages WHERE student_id = p_student_id;
+        DELETE FROM student_fluent_languages WHERE student_id = p_student_id;
+        DELETE FROM student_notes WHERE student_id = p_student_id;
+        DELETE FROM student_study_abroad_wishlist WHERE student_id = p_student_id;
+        DELETE FROM study_abroad WHERE student_id = p_student_id;
+        DELETE FROM students WHERE id = p_student_id;
+    END;
+$delete_student$ LANGUAGE plpgsql;
