0486407b1c30793abcf4b039e8e9936bbf2516c9
Andrei Nicholson <contact@andreinicholson.com>
2014-10-20 21:16:40 -0400

    Delete student stored procedure for next release

--- Schema/update-2.1.0.sql
+++ Schema/update-2.1.0.sql
@@ -30,3 +30,24 @@ CREATE INDEX IX_Exceptions_ErrorHash_AppName_CreationDate_DelDate ON exceptions 
 
 GRANT ALL PRIVILEGES ON exceptions TO neoanime_abroadadvisor;
 GRANT ALL PRIVILEGES ON exceptions_id_seq TO neoanime_abroadadvisor;
+
+CREATE OR REPLACE FUNCTION delete_student(INT) RETURNS VOID AS $delete_student$
+    DECLARE
+        p_student_id ALIAS FOR $1;
+    BEGIN
+        RAISE NOTICE 'Deleting student ID %', p_student_id;
+
+        IF NOT EXISTS (SELECT 1 FROM students WHERE id = p_student_id) THEN
+            RAISE EXCEPTION 'Student ID % not found', p_student_id;
+        END IF;
+
+        DELETE FROM event_log WHERE student_id = p_student_id;
+        DELETE FROM matriculation WHERE student_id = p_student_id;
+        DELETE FROM student_desired_languages WHERE student_id = p_student_id;
+        DELETE FROM student_fluent_languages WHERE student_id = p_student_id;
+        DELETE FROM student_notes WHERE student_id = p_student_id;
+        DELETE FROM student_study_abroad_wishlist WHERE student_id = p_student_id;
+        DELETE FROM study_abroad WHERE student_id = p_student_id;
+        DELETE FROM students WHERE id = p_student_id;
+    END;
+$delete_student$ LANGUAGE plpgsql;
--- Schema/update-next.sql
+++ Schema/update-next.sql
@@ -1,20 +1,44 @@
-CREATE OR REPLACE FUNCTION delete_student(INT) RETURNS VOID AS $delete_student$
-    DECLARE
-        p_student_id ALIAS FOR $1;
-    BEGIN
-        RAISE NOTICE 'Deleting student ID %', p_student_id;
+CREATE TABLE location (
+id              SERIAL,
+country_id      INT NOT NULL,
+address         VARCHAR(128),
+address2        VARCHAR(128),
+locality        VARCHAR(128),
+region          VARCHAR(128),
+postal_code     VARCHAR(32),
 
-        IF NOT EXISTS (SELECT 1 FROM students WHERE id = p_student_id) THEN
-            RAISE EXCEPTION 'Student ID % not found', p_student_id;
-        END IF;
+PRIMARY KEY (id),
+FOREIGN KEY (country_id) REFERENCES countries (id)
+);
 
-        DELETE FROM event_log WHERE student_id = p_student_id;
-        DELETE FROM matriculation WHERE student_id = p_student_id;
-        DELETE FROM student_desired_languages WHERE student_id = p_student_id;
-        DELETE FROM student_fluent_languages WHERE student_id = p_student_id;
-        DELETE FROM student_notes WHERE student_id = p_student_id;
-        DELETE FROM student_study_abroad_wishlist WHERE student_id = p_student_id;
-        DELETE FROM study_abroad WHERE student_id = p_student_id;
-        DELETE FROM students WHERE id = p_student_id;
-    END;
-$delete_student$ LANGUAGE plpgsql;
+COMMENT ON TABLE location IS 'Address information for a general location type';
+COMMENT ON COLUMN location.locality IS 'The locality. For example, Greensboro';
+COMMENT ON COLUMN location.region IS 'The region. For example, North Carolina';
+
+GRANT ALL PRIVILEGES ON location TO neoanime_abroadadvisor;
+GRANT ALL PRIVILEGES ON location_id_seq TO neoanime_abroadadvisor;
+
+
+CREATE TABLE events (
+id              SERIAL,
+title           VARCHAR(256) NOT NULL,
+title2          VARCHAR(256),
+title3          VARCHAR(256),
+location_id     INT NOT NULL,
+start_date      TIMESTAMP NOT NULL,
+end_date        TIMESTAMP,
+web_site        VARCHAR(2048),
+notes           VARCHAR(4096),
+tags            VARCHAR(1024),
+
+PRIMARY KEY (id),
+FOREIGN KEY (location_id) REFERENCES location (id)
+);
+
+COMMENT ON TABLE events IS 'CGS event log';
+COMMENT ON COLUMN events.start_date IS 'Starting date and time of event';
+COMMENT ON COLUMN events.end_date IS 'Ending date and time if the event was a range of days or NULL if it was a one-time event';
+COMMENT ON COLUMN events.tags IS 'Comma-delimited list of event tags';
+
+GRANT ALL PRIVILEGES ON events TO neoanime_abroadadvisor;
+GRANT ALL PRIVILEGES ON events_id_seq TO neoanime_abroadadvisor;
