tetsuo13/Belletrix

commit 143d11105392100cb423deeb62fa12b88cf5a782

Allow students to be associated with more than one promo
#14

AbroadAdvisor/Models/StudentPromoLog.cs
@@ -2,6 +2,7 @@
 using Npgsql;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Bennett.AbroadAdvisor.Models
 {
@@ -13,8 +14,133 @@ namespace Bennett.AbroadAdvisor.Models
         public int StudentId { get; set; }
         public DateTime Created { get; set; }
 
+        public static IEnumerable<StudentPromoLog> Get()
+        {
+            ApplicationCache cacheProvider = new ApplicationCache();
+            ICollection<StudentPromoLog> logs = cacheProvider.Get(CacheId, () => new List<StudentPromoLog>());
+
+            if (logs.Count == 0)
+            {
+                const string sql = @"
+                    SELECT  promo_id, student_id, created
+                    FROM    student_promo_log";
+
+                try
+                {
+                    using (NpgsqlConnection connection = new NpgsqlConnection(Connections.Database.Dsn))
+                    {
+                        connection.ValidateRemoteCertificateCallback += Connections.Database.connection_ValidateRemoteCertificateCallback;
+
+                        using (NpgsqlCommand command = connection.CreateCommand())
+                        {
+                            command.CommandText = sql;
+                            connection.Open();
+
+                            using (NpgsqlDataReader reader = command.ExecuteReader())
+                            {
+                                while (reader.Read())
+                                {
+                                    logs.Add(new StudentPromoLog()
+                                        {
+                                            PromoId = reader.GetInt32(reader.GetOrdinal("promo_id")),
+                                            StudentId = reader.GetInt32(reader.GetOrdinal("student_id")),
+                                            Created = reader.GetDateTime(reader.GetOrdinal("created"))
+                                        });
+                                }
+
+                                cacheProvider.Set(CacheId, logs);
+                            }
+                        }
+                    }
+                }
+                catch (Exception e)
+                {
+                    e.Data["SQL"] = sql;
+                    throw e;
+                }
+            }
+
+            return logs;
+        }
+
+        public static void Save(NpgsqlConnection connection, int studentId, IEnumerable<int> promoIds)
+        {
+            Delete(connection, studentId);
+
+            if (promoIds.Any())
+            {
+                ApplicationCache cacheProvider = new ApplicationCache();
+                ICollection<StudentPromoLog> logs = cacheProvider.Get(CacheId, () => new List<StudentPromoLog>());
+
+                const string sql = @"
+                    INSERT INTO student_promo_log
+                    (
+                        promo_id, student_id, created
+                    )
+                    VALUES
+                    (
+                        @PromoId, @StudentId, @Created
+                    )";
+
+                try
+                {
+                    using (NpgsqlCommand command = connection.CreateCommand())
+                    {
+                        DateTime created = DateTime.Now.ToUniversalTime();
+
+                        command.CommandText = sql;
+
+                        command.Parameters.Add("@PromoId", NpgsqlTypes.NpgsqlDbType.Integer);
+                        command.Parameters.Add("@StudentId", NpgsqlTypes.NpgsqlDbType.Integer).Value = studentId;
+                        command.Parameters.Add("@Created", NpgsqlTypes.NpgsqlDbType.Timestamp).Value = created;
+
+                        command.Prepare();
+
+                        foreach (int promoId in promoIds)
+                        {
+                            StudentPromoLog log = new StudentPromoLog()
+                            {
+                                PromoId = promoId,
+                                StudentId = studentId,
+                                Created = created
+                            };
+
+                            command.Parameters[0].Value = promoId;
+                            command.ExecuteNonQuery();
+
+                            logs.Add(log);
+                        }
+                    }
+
+                    cacheProvider.Set(CacheId, logs);
+                }
+                catch (Exception e)
+                {
+                    e.Data["SQL"] = sql;
+                    throw e;
+                }
+            }
+        }
+
+        public static IEnumerable<int> GetPromoIdsForStudent(int studentId)
+        {
+            IEnumerable<StudentPromoLog> logs = Get();
+            return logs
+                .Where(x => x.StudentId == studentId)
+                .Select(x => x.PromoId);
+        }
+
+        /// <summary>
+        /// Save a single student ID to promo code association.
+        /// </summary>
+        /// <param name="connection">Opened connection.</param>
+        /// <param name="studentId">Student ID.</param>
+        /// <param name="promoCode">Promo code.</param>
         public static void Save(NpgsqlConnection connection, int studentId, string promoCode)
         {
+            ApplicationCache cacheProvider = new ApplicationCache();
+            ICollection<StudentPromoLog> logs = cacheProvider.Get(CacheId, () => new List<StudentPromoLog>());
+
             const string sql = @"
                 INSERT INTO student_promo_log
                 (
@@ -30,14 +156,24 @@ namespace Bennett.AbroadAdvisor.Models
             {
                 using (NpgsqlCommand command = connection.CreateCommand())
                 {
+                    DateTime created = DateTime.Now.ToUniversalTime();
+
                     command.CommandText = sql;
 
                     command.Parameters.Add("@PromoCode", NpgsqlTypes.NpgsqlDbType.Varchar, 32).Value = promoCode.ToLower();
                     command.Parameters.Add("@StudentId", NpgsqlTypes.NpgsqlDbType.Integer).Value = studentId;
-                    command.Parameters.Add("@Created", NpgsqlTypes.NpgsqlDbType.Timestamp).Value = DateTime.Now.ToUniversalTime();
+                    command.Parameters.Add("@Created", NpgsqlTypes.NpgsqlDbType.Timestamp).Value = created;
 
                     int promoId = Convert.ToInt32(command.ExecuteScalar());
-                    CacheOurselves(promoId, studentId);
+
+                    logs.Add(new StudentPromoLog()
+                        {
+                            PromoId = promoId,
+                            StudentId = studentId,
+                            Created = created
+                        });
+
+                    cacheProvider.Set(CacheId, logs);
                 }
             }
             catch (Exception e)
@@ -45,121 +181,15 @@ namespace Bennett.AbroadAdvisor.Models
                 e.Data["SQL"] = sql;
                 throw e;
             }
-        }
-
-        public static void Save(NpgsqlConnection connection, int studentId, int promoId)
-        {
-            const string sql = @"
-                INSERT INTO student_promo_log
-                (
-                    promo_id, student_id, created
-                )
-                VALUES
-                (
-                    @PromoId, @StudentId, @Created
-                )";
-
-            try
-            {
-                using (NpgsqlCommand command = connection.CreateCommand())
-                {
-                    command.CommandText = sql;
-                    command.Parameters.Add("@PromoId", NpgsqlTypes.NpgsqlDbType.Integer).Value = promoId;
-                    command.Parameters.Add("@StudentId", NpgsqlTypes.NpgsqlDbType.Integer).Value = studentId;
-                    command.Parameters.Add("@Created", NpgsqlTypes.NpgsqlDbType.Timestamp).Value = DateTime.Now.ToUniversalTime();
-                    command.ExecuteNonQuery();
-
-                    CacheOurselves(promoId, studentId);
-                }
-            }
-            catch (Exception e)
-            {
-                e.Data["SQL"] = sql;
-                throw e;
-            }
-        }
-
-        private static void CacheOurselves(int promoId, int studentId)
-        {
-            ApplicationCache cacheProvider = new ApplicationCache();
-            List<StudentPromoLog> events = cacheProvider.Get(CacheId, () => new List<StudentPromoLog>());
-            events.Add(new StudentPromoLog()
-                {
-                    PromoId = promoId,
-                    StudentId = studentId
-                });
-            cacheProvider.Set(CacheId, events);
-        }
-
-        /// <summary>
-        /// Change the associated promo ID for an existing student.
-        /// </summary>
-        /// <param name="studentId">Student ID.</param>
-        /// <param name="promoId">Promo ID.</param>
-        private static void UpdateCache(int studentId, int promoId)
-        {
-            ApplicationCache cacheProvider = new ApplicationCache();
-            List<StudentPromoLog> events = cacheProvider.Get(CacheId, () => new List<StudentPromoLog>());
-
-            foreach (StudentPromoLog e in events)
-            {
-                if (e.StudentId == studentId)
-                {
-                    e.PromoId = promoId;
-                    break;
-                }
-            }
-
-            cacheProvider.Set(CacheId, events);
         }
 
         public static IEnumerable<StudentPromoLog> GetLogsForPromo(int id)
         {
-            ICollection<StudentPromoLog> logs = new List<StudentPromoLog>();
-            const string sql = @"
-                SELECT  student_id, created
-                FROM    student_promo_log
-                WHERE   promo_id = @PromoId";
-
-            try
-            {
-                using (NpgsqlConnection connection = new NpgsqlConnection(Connections.Database.Dsn))
-                {
-                    connection.ValidateRemoteCertificateCallback += Connections.Database.connection_ValidateRemoteCertificateCallback;
-
-                    using (NpgsqlCommand command = connection.CreateCommand())
-                    {
-                        command.CommandText = sql;
-                        command.Parameters.Add("@PromoId", NpgsqlTypes.NpgsqlDbType.Integer).Value = id;
-                        connection.Open();
-
-                        using (NpgsqlDataReader reader = command.ExecuteReader())
-                        {
-                            while (reader.Read())
-                            {
-                                StudentPromoLog log = new StudentPromoLog()
-                                {
-                                    PromoId = id,
-                                    StudentId = reader.GetInt32(reader.GetOrdinal("student_id")),
-                                    Created = reader.GetDateTime(reader.GetOrdinal("created"))
-                                };
-
-                                logs.Add(log);
-                            }
-                        }
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                e.Data["SQL"] = sql;
-                throw e;
-            }
-
-            return logs;
+            IEnumerable<StudentPromoLog> logs = Get();
+            return logs.Where(x => x.PromoId == id);
         }
 
-        public static void Delete(NpgsqlConnection connection, int studentId)
+        private static void Delete(NpgsqlConnection connection, int studentId)
         {
             const string sql = @"
                 DELETE FROM student_promo_log
@@ -179,69 +209,11 @@ namespace Bennett.AbroadAdvisor.Models
                 e.Data["SQL"] = sql;
                 throw e;
             }
-        }
 
-        /// <summary>
-        /// Check to see if the student is already associated with a promo. If
-        /// not then create a new association; otherwise update the promo ID
-        /// that exists for the student.
-        /// </summary>
-        /// <param name="connection">Opened connection.</param>
-        /// <param name="studentId">Student ID.</param>
-        /// <param name="promoId">Promo ID.</param>
-        public static void Upsert(NpgsqlConnection connection, int studentId, int promoId)
-        {
-            string sql = null;
-
-            try
-            {
-                bool recordExists = false;
-
-                using (NpgsqlCommand command = connection.CreateCommand())
-                {
-                    sql = @"
-                        SELECT  promo_id
-                        FROM    student_promo_log
-                        WHERE   student_id = @StudentId";
-
-                    command.CommandText = sql;
-                    command.Parameters.Add("@StudentId", NpgsqlTypes.NpgsqlDbType.Numeric).Value = studentId;
-
-                    using (NpgsqlDataReader reader = command.ExecuteReader())
-                    {
-                        recordExists = reader.HasRows;
-                    }
-                }
-
-                if (recordExists)
-                {
-                    using (NpgsqlCommand command = connection.CreateCommand())
-                    {
-                        sql = @"
-                            UPDATE  student_promo_log
-                            SET     promo_id = @PromoId,
-                                    created = @Created
-                            WHERE   student_id = @StudentId";
-
-                        command.CommandText = sql;
-                        command.Parameters.Add("@PromoId", NpgsqlTypes.NpgsqlDbType.Numeric).Value = promoId;
-                        command.Parameters.Add("@Created", NpgsqlTypes.NpgsqlDbType.Timestamp).Value = DateTime.Now.ToUniversalTime();
-                        command.Parameters.Add("@StudentId", NpgsqlTypes.NpgsqlDbType.Numeric).Value = studentId;
-                        command.ExecuteNonQuery();
-
-                        UpdateCache(studentId, promoId);
-                    }
-                }
-                else
-                {
-                    Save(connection, studentId, promoId);
-                }
-            }
-            catch (Exception e)
-            {
-                e.Data["SQL"] = sql;
-                throw e;
-            }
+            ApplicationCache cacheProvider = new ApplicationCache();
+            IEnumerable<StudentPromoLog> logs = cacheProvider.Get(CacheId, () => new List<StudentPromoLog>());
+            logs = logs.Where(x => x.StudentId != studentId);
+            cacheProvider.Set(CacheId, logs);
         }
     }
 }

AbroadAdvisor/Controllers/CacheController.cs
@@ -26,6 +26,7 @@ namespace Bennett.AbroadAdvisor.Controllers
             StudentModel.GetStudents();
             StudyAbroadModel.GetAll();
             PromoModel.GetPromos(true);
+            StudentPromoLog.Get();
 
             TimeSpan span = DateTime.Now - startDate;
 

AbroadAdvisor/Controllers/StudentController.cs
@@ -213,10 +213,7 @@ namespace Bennett.AbroadAdvisor.Controllers
                 }));
 
             ViewBag.StudyAbroadPlaces = places;
-
-            IEnumerable<object> promos = PromoModel.AsSources()
-                .Select(x => new { Id = x.Id, Description = String.Format("{0} ({1})", x.Description, x.Code) });
-            ViewBag.Promos = new SelectList(promos, "Id", "Description");
+            ViewBag.Promos = PromoModel.AsSources();
         }
 
         private void PrepareStudyAbroadDropDowns()

AbroadAdvisor/Models/StudentModel.cs
@@ -18,8 +18,11 @@ namespace Bennett.AbroadAdvisor.Models
 
         public int NumberOfNotes { get; set; }
 
+        /// <summary>
+        /// Set of promos that the student may be associated with.
+        /// </summary>
         [Display(Name = "Promo")]
-        public int? PromoId { get; set; }
+        public IEnumerable<int> PromoIds { get; set; }
 
         public void SaveChanges(UserModel user)
         {
@@ -76,14 +79,7 @@ namespace Bennett.AbroadAdvisor.Models
                     SaveStudentLanguages(connection, Id, "student_desired_languages", SelectedDesiredLanguages);
                     SaveStudentLanguages(connection, Id, "student_studied_languages", StudiedLanguages);
 
-                    if (!PromoId.HasValue)
-                    {
-                        StudentPromoLog.Delete(connection, Id);
-                    }
-                    else
-                    {
-                        StudentPromoLog.Upsert(connection, Id, PromoId.Value);
-                    }
+                    StudentPromoLog.Save(connection, Id, PromoIds);
 
                     EventLogModel eventLog = new EventLogModel()
                     {
@@ -119,13 +115,11 @@ namespace Bennett.AbroadAdvisor.Models
                                         s.campus_email, s.alternate_email, s.graduating_year,
                                         s.classification, s.street_address, s.street_address2,
                                         s.city, s.state, s.postal_code,
-                                        s.entering_year, promo_id,
+                                        s.entering_year,
                                         COUNT(n.id) AS num_notes
                     FROM                students s
                     LEFT OUTER JOIN     student_notes n ON
                                         s.id = n.student_id
-                    LEFT OUTER JOIN     student_promo_log p ON
-                                        s.id = p.student_id
                     GROUP BY            s.id, s.created, s.initial_meeting,
                                         s.first_name, s.middle_name, s.last_name,
                                         s.living_on_campus, s.phone_number, s.student_id,
@@ -134,7 +128,7 @@ namespace Bennett.AbroadAdvisor.Models
                                         s.campus_email, s.alternate_email, s.graduating_year,
                                         s.classification, s.street_address, s.street_address2,
                                         s.city, s.state, s.postal_code,
-                                        s.entering_year, promo_id
+                                        s.entering_year
                     ORDER BY            s.last_name, s.first_name";
 
                 IList<StudentModel> studentList = new List<StudentModel>();
@@ -199,11 +193,7 @@ namespace Bennett.AbroadAdvisor.Models
                                         student.InitialMeeting = DateTimeFilter.UtcToLocal(reader.GetDateTime(ord));
                                     }
 
-                                    ord = reader.GetOrdinal("promo_id");
-                                    if (!reader.IsDBNull(ord))
-                                    {
-                                        student.PromoId = reader.GetInt32(ord);
-                                    }
+                                    student.PromoIds = StudentPromoLog.GetPromoIdsForStudent(student.Id);
 
                                     studentList.Add(student);
                                 }
@@ -253,7 +243,7 @@ namespace Bennett.AbroadAdvisor.Models
         public static IEnumerable<StudentModel> FromPromo(int promoId)
         {
             IEnumerable<StudentModel> students = GetStudents();
-            return students.Where(x => x.PromoId.HasValue && x.PromoId.Value == promoId);
+            return students.Where(x => x.PromoIds != null && x.PromoIds.Any(y => y == promoId));
         }
 
         private static void PopulateStudentLanguages(NpgsqlConnection connection, ref IList<StudentModel> students)
@@ -434,14 +424,7 @@ namespace Bennett.AbroadAdvisor.Models
                 {
                     base.Save(connection, user.Id);
 
-                    if (!PromoId.HasValue)
-                    {
-                        StudentPromoLog.Delete(connection, Id);
-                    }
-                    else
-                    {
-                        StudentPromoLog.Upsert(connection, Id, PromoId.Value);
-                    }
+                    StudentPromoLog.Save(connection, Id, PromoIds);
 
                     EventLogModel eventLog = new EventLogModel()
                     {

AbroadAdvisor/Views/Student/Add.cshtml
@@ -127,8 +127,8 @@
                             </div>
                             <div class="col-lg-6">
                                 <div class="form-group">
-                                    @Html.LabelFor(s => s.PromoId)
-                                    @Html.DropDownListFor(s => s.PromoId, ViewBag.Promos as SelectList, "Please Select", new { @class = "form-control" })
+                                    @Html.LabelFor(s => s.PromoIds)
+                                    @Html.ListBoxFor(s => s.PromoIds, new MultiSelectList(ViewBag.Promos, "Id", "Description"), new { @class = "multiselect", multiple = "multiple" })
                                 </div>
                             </div>
                         </div>

AbroadAdvisor/Views/Student/Edit.cshtml
@@ -162,8 +162,8 @@
                                     </div>
                                     <div class="col-lg-6">
                                         <div class="form-group">
-                                            @Html.LabelFor(s => s.PromoId)
-                                            @Html.DropDownListFor(s => s.PromoId, ViewBag.Promos as SelectList, "Please Select", new { @class = "form-control" })
+                                            @Html.LabelFor(s => s.PromoIds)
+                                            @Html.ListBoxFor(s => s.PromoIds, new MultiSelectList(ViewBag.Promos, "Id", "Description"), new { @class = "multiselect", multiple = "multiple" })
                                         </div>
                                     </div>
                                 </div>

Schema/schema.sql
@@ -615,14 +615,15 @@ promo_id    INT NOT NULL,
 student_id  INT NOT NULL,
 created     TIMESTAMP NOT NULL,
 
-PRIMARY KEY (student_id),
+PRIMARY KEY (promo_id, student_id),
 FOREIGN KEY (promo_id) REFERENCES user_promo (id),
 FOREIGN KEY (student_id) REFERENCES students (id)
 );
 
-COMMENT ON TABLE student_promo_log IS 'Students created through ptomos';
+COMMENT ON TABLE student_promo_log IS 'Students associated with promos';
 
 CREATE INDEX student_promo_log_idx1 ON student_promo_log (promo_id);
+CREATE INDEX student_promo_log_idx2 ON student_promo_log (student_id);
 
 GRANT ALL PRIVILEGES ON student_promo_log TO neoanime_abroadadvisor;
 

Schema/update-2.4.0.sql
@@ -1,0 +1,6 @@
+﻿COMMENT ON TABLE student_promo_log IS 'Students associated with promos';
+
+-- A student may be associated with more than one promo.
+ALTER TABLE student_promo_log DROP CONSTRAINT student_promo_log_pkey;
+ALTER TABLE student_promo_log ADD PRIMARY KEY (promo_id, student_id);
+CREATE INDEX student_promo_log_idx2 ON student_promo_log (student_id);

commit 6f72c2d535f0bdfc7aeff821b0ce266edea404b3

Fix null error introduced in 143d11105392100cb423deeb62fa12b88cf5a782
#23

AbroadAdvisor/Models/StudentPromoLog.cs
@@ -67,7 +67,7 @@ namespace Bennett.AbroadAdvisor.Models
         {
             Delete(connection, studentId);
 
-            if (promoIds.Any())
+            if (promoIds != null && promoIds.Any())
             {
                 ApplicationCache cacheProvider = new ApplicationCache();
                 ICollection<StudentPromoLog> logs = cacheProvider.Get(CacheId, () => new List<StudentPromoLog>());