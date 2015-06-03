bda18100f9ada90cf0308f204d82130b067a0d74
Andrei Nicholson <contact@andreinicholson.com>
2015-06-02 23:37:17 -0400

    Fix exceptions caused by analytics tracking breaking our application

--- Belletrix.Core/Analytics.cs
+++ Belletrix.Core/Analytics.cs
@@ -6,17 +6,16 @@ using System.Web;
 namespace Belletrix.Core
 {
     /// <summary>
-    /// Record analytics server-side. Only functions when not in DEBUG.
+    /// Record analytics server-side. Only functions when not running a Debug
+    /// build. Intended to be a "fire and forget" API.
     /// </summary>
     /// <remarks>
-    /// <para>
     /// We use nginx to distribute requests between multiple servers.
-    /// Because of this setup, the <c>Request.UserHostAddress</c> will be the
+    /// Because of this setup, the <b>Request.UserHostAddress</b> will be the
     /// IP address of our load-balancer instead of the IP address of the
     /// remote user. You can use
-    /// <c>Request.ServerVariables["HTTP_X_FORWARDED_FOR"]</c> to access the
+    /// <b>Request.ServerVariables["HTTP_X_FORWARDED_FOR"]</b> to access the
     /// user's IP address.
-    /// </para>
     /// </remarks>
     /// <seealso href="http://support.appharbor.com/kb/getting-started/information-about-our-load-balancer">
     /// Information about our load-balancer
@@ -30,52 +29,59 @@ namespace Belletrix.Core
                 return;
             }
 
-            PiwikTracker.URL = "http://analytics.andreinicholson.com";
-            PiwikTracker tracker = new PiwikTracker(17);
-
             try
             {
-                string token = ConfigurationManager.AppSettings["AnalyticsAdminToken"];
+                PiwikTracker.URL = "http://analytics.andreinicholson.com";
+                PiwikTracker tracker = new PiwikTracker(17);
 
-                if (!String.IsNullOrEmpty(token))
+                try
                 {
-                    tracker.setTokenAuth(token);
+                    string token = ConfigurationManager.AppSettings["AnalyticsAdminToken"];
 
-                    try
+                    if (!String.IsNullOrEmpty(token))
                     {
-                        tracker.setIp(request.ServerVariables["HTTP_X_FORWARDED_FOR"]);
-                    }
-                    catch (Exception)
-                    {
-                        tracker.setIp(request.UserHostAddress);
+                        tracker.setTokenAuth(token);
+
+                        try
+                        {
+                            tracker.setIp(request.ServerVariables["HTTP_X_FORWARDED_FOR"]);
+                        }
+                        catch (Exception)
+                        {
+                            tracker.setIp(request.UserHostAddress);
+                        }
                     }
                 }
+                catch (Exception)
+                {
+                }
+
+                if (request.UserLanguages != null)
+                {
+                    tracker.setBrowserLanguage(request.UserLanguages[0]);
+                }
+
+                tracker.setUrl(request.Url.Scheme + "://" + request.Url.Host + request.Url.PathAndQuery);
+                tracker.setBrowserHasCookies(request.Cookies.Count > 0);
+                tracker.setUserAgent(request.UserAgent);
+                tracker.setRequestTimeout(600000);
+
+                if (request.UrlReferrer != null)
+                {
+                    tracker.setUrlReferrer(request.UrlReferrer.ToString());
+                }
+
+                if (!String.IsNullOrEmpty(username))
+                {
+                    tracker.setCustomVariable(1, "username", username);
+                }
+
+                tracker.doTrackPageView(pageTitle);
             }
             catch (Exception)
             {
+                // TODO: Log it?
             }
-
-            if (request.UserLanguages != null)
-            {
-                tracker.setBrowserLanguage(request.UserLanguages[0]);
-            }
-
-            tracker.setUrl(request.Url.Scheme + "://" + request.Url.Host + request.Url.PathAndQuery);
-            tracker.setBrowserHasCookies(request.Cookies.Count > 0);
-            tracker.setUserAgent(request.UserAgent);
-            tracker.setRequestTimeout(600000);
-
-            if (request.UrlReferrer != null)
-            {
-                tracker.setUrlReferrer(request.UrlReferrer.ToString());
-            }
-
-            if (!String.IsNullOrEmpty(username))
-            {
-                tracker.setCustomVariable(1, "username", username);
-            }
-
-            tracker.doTrackPageView(pageTitle);
         }
     }
 }
