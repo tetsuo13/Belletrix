88c8c2e1d2fe2efb84c0acb1174a185b4a726858
Andrei Nicholson <contact@andreinicholson.com>
2015-06-02 23:52:18 -0400

    Work on schema

--- Schema/update-next.sql
+++ Schema/update-next.sql
@@ -19,7 +19,16 @@ GRANT ALL PRIVILEGES ON location TO neoanime_abroadadvisor;
 GRANT ALL PRIVILEGES ON location_id_seq TO neoanime_abroadadvisor;
 
 
-CREATE TABLE events (
+CREATE TABLE event_log_person (
+id              SERIAL,
+full_name       VARCHAR(128) NOT NULL,
+description     VARCHAR(256),
+phone           VARCHAR(32),
+email           VARCHAR(128)
+);
+
+
+CREATE TABLE event_log (
 id              SERIAL,
 title           VARCHAR(256) NOT NULL,
 title2          VARCHAR(256),
@@ -27,9 +36,9 @@ title3          VARCHAR(256),
 location_id     INT NOT NULL,
 start_date      TIMESTAMP NOT NULL,
 end_date        TIMESTAMP,
+on_campus       BOOLEAN NOT NULL,
 web_site        VARCHAR(2048),
 notes           VARCHAR(4096),
-tags            VARCHAR(1024),
 
 PRIMARY KEY (id),
 FOREIGN KEY (location_id) REFERENCES location (id)
@@ -42,3 +51,24 @@ COMMENT ON COLUMN events.tags IS 'Comma-delimited list of event tags';
 
 GRANT ALL PRIVILEGES ON events TO neoanime_abroadadvisor;
 GRANT ALL PRIVILEGES ON events_id_seq TO neoanime_abroadadvisor;
+
+
+CREATE TABLE event_log_types (
+event_id        INT NOT NULL,
+type_name       VARCHAR(256) NOT NULL,
+
+PRIMARY KEY (event_id, type_name),
+FOREIGN KEY (event_id) REFERENCES event_log (id)
+);
+
+
+CREATE TABLE event_log_attendees (
+event_id        INT NOT NULL,
+person_id       INT NOT NULL,
+);
+
+
+CREATE TABLE event_log_contacts (
+event_id        INT NOT NULL,
+person_id       INT NOT NULL,
+);
