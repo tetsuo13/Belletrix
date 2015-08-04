d2d6aebf1fd7916fd66ca02f2ed4189e462a9b49
Andrei Nicholson <contact@andreinicholson.com>
2015-08-03 20:57:13 -0400

    HTTPS for the win!

--- Belletrix/Global.asax.cs
+++ Belletrix/Global.asax.cs
@@ -2,6 +2,7 @@
 using StackExchange.Exceptional;
 using System;
 using System.Web;
+using System.Web.Helpers;
 using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
@@ -33,10 +34,45 @@ namespace Belletrix
             }
         }
 
+        /// <summary>
+        /// Configures the anti-forgery tokens. See
+        /// http://www.asp.net/mvc/overview/security/xsrfcsrf-prevention-in-aspnet-mvc-and-web-pages
+        /// </summary>
+        private static void ConfigureAntiForgeryTokens()
+        {
+            // Rename the Anti-Forgery cookie from "__RequestVerificationToken" to "f". This adds a little security
+            // through obscurity and also saves sending a few characters over the wire. Sadly there is no way to change
+            // the form input name which is hard coded in the @Html.AntiForgeryToken helper and the
+            // ValidationAntiforgeryTokenAttribute to  __RequestVerificationToken.
+            // <input name="__RequestVerificationToken" type="hidden" value="..." />
+            AntiForgeryConfig.CookieName = "f";
+
+            // If you have enabled SSL. Uncomment this line to ensure that the Anti-Forgery
+            // cookie requires SSL to be sent across the wire.
+#if !DEBUG
+            AntiForgeryConfig.RequireSsl = true;
+#endif
+        }
+
+        /// <summary>
+        /// Configures the view engines. By default, Asp.Net MVC includes the Web Forms (WebFormsViewEngine) and 
+        /// Razor (RazorViewEngine) view engines. You can remove view engines you are not using here for better
+        /// performance.
+        /// </summary>
+        private static void ConfigureViewEngines()
+        {
+            // Only use the RazorViewEngine.
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+        }
+
         protected void Application_Start()
         {
             // Disable the X-AspNetMvc-Version header.
             MvcHandler.DisableMvcResponseHeader = true;
+
+            ConfigureAntiForgeryTokens();
+            ConfigureViewEngines();
 
             AreaRegistration.RegisterAllAreas();
 
@@ -45,10 +81,6 @@ namespace Belletrix
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             GlobalConfiguration.Configuration.Filters.Add(new System.Web.Http.AuthorizeAttribute());
-
-            // Remove Web Form (ASPX) view engine, only Razor.
-            ViewEngines.Engines.Clear();
-            ViewEngines.Engines.Add(new RazorViewEngine());
         }
     }
 }
