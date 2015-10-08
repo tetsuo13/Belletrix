f258c67e2344d3171e5b829ef1a33acdce6be216
Andrei Nicholson <contact@andreinicholson.com>
2015-10-07 22:33:45 -0400

    Alphabetize registrations since order doesn't matter

--- src/Belletrix/App_Start/UnityConfig.cs
+++ src/Belletrix/App_Start/UnityConfig.cs
@@ -24,22 +24,23 @@ namespace Belletrix.App_Start
                 new InjectionConstructor(Connections.Database.Dsn));
 
             container.RegisterType<IActivityService, ActivityService>();
+            container.RegisterType<IEventLogService, EventLogService>();
+            container.RegisterType<IPingService, PingService>();
+            container.RegisterType<IPromoService, PromoService>();
+            container.RegisterType<IStudentNoteService, StudentNoteService>();
+            container.RegisterType<IStudentService, StudentService>();
+            container.RegisterType<IStudyAbroadService, StudyAbroadService>();
+            container.RegisterType<IUserService, UserService>();
+
             container.RegisterType<IActivityLogRepository, ActivityLogRepository>();
             container.RegisterType<IActivityLogPersonRepository, ActivityLogPersonRepository>();
             container.RegisterType<IEventLogRepository, EventLogRepository>();
-            container.RegisterType<IEventLogService, EventLogService>();
-            container.RegisterType<IUserRepository, UserRepository>();
-            container.RegisterType<IUserService, UserService>();
             container.RegisterType<IPingRepository, PingRepository>();
-            container.RegisterType<IPingService, PingService>();
             container.RegisterType<IPromoRepository, PromoRepository>();
-            container.RegisterType<IPromoService, PromoService>();
             container.RegisterType<IStudentNoteRepository, StudentNoteRepository>();
-            container.RegisterType<IStudentNoteService, StudentNoteService>();
             container.RegisterType<IStudentRepository, StudentRepository>();
-            container.RegisterType<IStudentService, StudentService>();
             container.RegisterType<IStudyAbroadRepository, StudyAbroadRepository>();
-            container.RegisterType<IStudyAbroadService, StudyAbroadService>();
+            container.RegisterType<IUserRepository, UserRepository>();
         }
     }
 }
