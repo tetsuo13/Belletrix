1a4e3edf23e89685bafd6c31dbdbf40e76dad347
Andrei Nicholson <contact@andreinicholson.com>
2015-12-09 22:03:53 -0500

    Move Entity test off into its own project

---
+++ src/Belletrix.Entity.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Belletrix.Entity.Tests")]
+[assembly: AssemblyProduct("Belletrix.Entity.Tests")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("becad3ea-c938-4a5e-9099-5d3ec12cf27c")]
