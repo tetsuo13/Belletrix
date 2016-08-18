58cde1014eee6f17ef662ec34a13881cd05b219c
Andrei Nicholson <contact@andreinicholson.com>
2016-08-17 20:33:47 -0400

    Remove obsolete note

--- src/Belletrix.DAL/UnitOfWork.cs
+++ src/Belletrix.DAL/UnitOfWork.cs
@@ -4,25 +4,6 @@ using System.Data.SqlClient;
 
 namespace Belletrix.DAL
 {
-    /// <summary>
-    /// 
-    /// </summary>
-    /// <remarks>
-    /// If there is an uncaught exception in the application anywhere, the
-    /// Dispose() method doesn't end up rolling back the transaction. When the
-    /// user revisits the web site they will end up with the following error:
-    /// 
-    /// <code>
-    /// Npgsql.NpgsqlException:
-    /// current transaction is aborted, commands ignored until end of transaction block
-    /// Severity: ERROR
-    /// Code: 25P02
-    /// </code>
-    /// 
-    /// This can be fixed by restarting the application on the server.
-    /// Ultimately this is an indication of bad design on the UnitOfWork
-    /// implementation with regard to transaction scope.
-    /// </remarks>
     public class UnitOfWork : IUnitOfWork
     {
         private IDbConnection context;
