tetsuo13/Belletrix

commit 1c6a03cdeddcb6668cf5d3fe9109ac45c868bf51

Correctness for correctness sake

src/Belletrix.Web/Scripts/App/ActivityLog.js
@@ -76,7 +76,7 @@ var Belletrix;
                             dataType: "json",
                             url: participantsUrl,
                             cache: false,
-                            success: function (data, textStatus, jqXHR) {
+                            success: function (data) {
                                 $.each(data, function (index, value) {
                                     self_1.addParticipantRow(value.Person.FullName, value.Person.Id);
                                 });

src/Belletrix.Web/Scripts/App/ActivityLog.ts
@@ -67,7 +67,7 @@
          * @param populateSessionUrl URL to populate existing participants into the session for an existing activity.
          * @param participantsUrl URL to fetch existing participants.
          */
-        initSession(startSessionUrl: string, populateSessionUrl?: string, participantsUrl?: string): void {
+        public initSession(startSessionUrl: string, populateSessionUrl?: string, participantsUrl?: string): void {
             $.ajax({
                 url: startSessionUrl,
                 cache: false
@@ -88,8 +88,8 @@
                             dataType: "json",
                             url: participantsUrl,
                             cache: false,
-                            success: function (data, textStatus, jqXHR) {
-                                $.each(data, function (index, value) {
+                            success: function (data): void {
+                                $.each(data, function (index, value): void {
                                     self.addParticipantRow(value.Person.FullName, value.Person.Id);
                                 });
                             }
@@ -118,7 +118,7 @@
             if (personSelect.length) {
                 let self = this;
 
-                submitButton.click(function (event: JQueryEventObject) {
+                submitButton.click(function (event: JQueryEventObject): void {
                     event.preventDefault();
 
                     $.ajax({
@@ -129,7 +129,7 @@
                             type: typeSelect.val(),
                             sessionId: self._sessionId
                         },
-                        success: function (result: any) {
+                        success: function (result: any): void {
                             if (!result.hasOwnProperty("Success") ||
                                 !result.hasOwnProperty("Message")) {
                                 Belletrix.Common.errorMessage("Unexpected result. Try again?\n" + result);
@@ -166,14 +166,14 @@
 
             $.validator.unobtrusive.parse(modalDialogForm);
 
-            modalDialogForm.submit(function (event: JQueryEventObject) {
+            modalDialogForm.submit(function (event: JQueryEventObject): void {
                 event.preventDefault();
 
                 $.ajax({
                     url: this.action,
                     type: this.method,
                     data: $(this).serialize(),
-                    success: function (result: any) {
+                    success: function (result: any): void {
                         if (!result.hasOwnProperty("Success") ||
                             !result.hasOwnProperty("Message") ||
                             !result.hasOwnProperty("Id")) {
@@ -213,13 +213,13 @@
                     id: id,
                     sessionId: self._sessionId
                 },
-                success: function (result: any) {
+                success: function (result: any): void {
                     if (!result.hasOwnProperty("Success") ||
                         !result.hasOwnProperty("Message")) {
                         Belletrix.Common.errorMessage("Unexpected result. Try again?\n" + result);
 
                     } else if (result.Success === true) {
-                        $("#participant-" + id).fadeOut(300, function () {
+                        $("#participant-" + id).fadeOut(300, function (): void {
                             $(this).remove();
                         });
 

src/Belletrix.Web/Scripts/App/Common.js
@@ -81,6 +81,12 @@ var Belletrix;
             return (Math.random() + 1).toString(36).slice(2);
         };
         ;
+        /**
+         * Make generic ajax request to delete a resource.
+         * @param deleteUrl URL to call.
+         * @param deleteId Resource ID to delete.
+         * @param successCallback Function to call after a successful ajax request.
+         */
         Common.handleDeleteCall = function (deleteUrl, deleteId, successCallback) {
             $.ajax({
                 method: "DELETE",

src/Belletrix.Web/Scripts/App/Common.ts
@@ -10,13 +10,13 @@ module Belletrix {
         /** Number of milliseconds between pinging the server. */
         private static _idleKillerInterval: number = 1000 * 60 * 10;
 
-        static UserFirstName: string;
-        static UserLastName: string;
+        public static UserFirstName: string;
+        public static UserLastName: string;
 
         /**
          * Disable the form submit button after clicking it.
          */
-        static singleSubmit(): void {
+        public static singleSubmit(): void {
             $("form").submit(function () {
                 // We're going to disable all submit buttons unless otherwise
                 // told to.
@@ -40,7 +40,7 @@ module Belletrix {
          * Show an error message as a modal dialog.
          * @param message Error message.
          */
-        static errorMessage(message: string): void {
+        public static errorMessage(message: string): void {
             const setup: string = `
 <div class="modal fade">
     <div class="modal-dialog">
@@ -68,8 +68,8 @@ module Belletrix {
          * application pool from shutting down after a period of inactivity.
          * @param pingUrl
          */
-        static initPinger(pingUrl: string): void {
-            setInterval(function () {
+        public static initPinger(pingUrl: string): void {
+            setInterval(function (): void {
                 $.ajax({
                     url: pingUrl,
                     cache: false,
@@ -83,7 +83,7 @@ module Belletrix {
          * @param numberToDisplay Number of selected elements to summarize.
          * @param maxHeight Maximum height in pixels.
          */
-        static initMultiselect(numberToDisplay: number, maxHeight?: number): void {
+        public static initMultiselect(numberToDisplay: number, maxHeight?: number): void {
             let options: any = {
                 numberDisplayed: numberToDisplay,
                 buttonContainer: '<div class="button-default" />',
@@ -101,17 +101,23 @@ module Belletrix {
          * Adds the "form-control" Bootstrap class to any input types that
          * were generated with the MVC EditorFor() HTML helper.
          */
-        static handleMvcEditor(): void {
+        public static handleMvcEditor(): void {
             $("input[type=email]").addClass("form-control");
         }
 
         /**
          * Generate a random string.
          */
-        static randomString(): string {
+        public static randomString(): string {
             return (Math.random() + 1).toString(36).slice(2);
         };
 
+        /**
+         * Make generic ajax request to delete a resource.
+         * @param deleteUrl URL to call.
+         * @param deleteId Resource ID to delete.
+         * @param successCallback Function to call after a successful ajax request.
+         */
         public static handleDeleteCall(deleteUrl: string, deleteId: number, successCallback: Function): void {
             $.ajax({
                 method: "DELETE",

src/Belletrix.Web/Scripts/App/StudentBase.js
@@ -18,7 +18,6 @@ var Belletrix;
         /**
          * Standard initialization for internal student form and public promo
          * form.
-         *
          * @param nameCheckUrl URL for unique name check.
          */
         StudentBase.prototype.initForm = function (nameCheckUrl) {
@@ -29,7 +28,6 @@ var Belletrix;
         };
         /**
          * Enable/disable all form fields.
-         *
          * @param disabled True to disable all fields.
          */
         StudentBase.prototype.toggleAllFormFields = function (disabled) {

src/Belletrix.Web/Scripts/App/StudentBase.ts
@@ -20,7 +20,6 @@ module Belletrix {
         /**
          * Standard initialization for internal student form and public promo
          * form.
-         *
          * @param nameCheckUrl URL for unique name check.
          */
         protected initForm(nameCheckUrl: string): void {
@@ -33,7 +32,6 @@ module Belletrix {
 
         /**
          * Enable/disable all form fields.
-         *
          * @param disabled True to disable all fields.
          */
         private toggleAllFormFields(disabled: boolean): void {

commit 13ff6fa861aeae1cb24507c8165164c458ca4a4f

Remove returning anything to match function return type void
Caused by 1c6a03cdeddcb6668cf5d3fe9109ac45c868bf51

src/Belletrix.Web/Scripts/App/ActivityLog.js
@@ -173,7 +173,6 @@ var Belletrix;
                         }
                     }
                 });
-                return false;
             });
         };
         /**
@@ -232,3 +231,4 @@ var Belletrix;
     }());
     Belletrix.ActivityLog = ActivityLog;
 })(Belletrix || (Belletrix = {}));
+//# sourceMappingURL=ActivityLog.js.map

src/Belletrix.Web/Scripts/App/ActivityLog.js.map
@@ -1,1 +1,1 @@
-{"version":3,"file":"ActivityLog.js","sourceRoot":"","sources":["ActivityLog.ts"],"names":[],"mappings":"AAAA,IAAO,SAAS,CAsQf;AAtQD,WAAO,SAAS,EAAC,CAAC;IACd;QAwBI;;;;;;WAMG;QACH,qBAAY,YAAoB,EAAE,cAAsB,EAAE,iBAAyB,EAAE,SAAiB;YA9BtG,wEAAwE;YAChE,+BAA0B,GAAW,qBAAqB,CAAC;YAE3D,8BAAyB,GAAW,6BAA6B,CAAC;YAE1E,gDAAgD;YACxC,6BAAwB,GAAG;gBAC/B,mDAAmD;gBACnD,gBAAgB,EAAE,uBAAuB;gBAEzC,iDAAiD;gBACjD,cAAc,EAAE,qBAAqB;gBAErC,yDAAyD;gBACzD,YAAY,EAAE,uBAAuB;aACxC,CAAC;YAEF,+DAA+D;YACvD,+BAA0B,GAAW,EAAE,CAAC;YAEhD,2BAA2B;YACnB,eAAU,GAAW,EAAE,CAAC;YAU5B,IAAI,CAAC,0BAA0B,GAAG,iBAAiB,CAAC;YACpD,IAAI,CAAC,UAAU,GAAG,SAAS,CAAC;YAE5B,IAAI,IAAI,GAAG,IAAI,CAAC;YAEhB,SAAS,CAAC,MAAM,CAAC,eAAe,EAAE,CAAC;YACnC,CAAC,CAAC,sBAAsB,CAAC,CAAC,UAAU,EAAE,CAAC;YACvC,SAAS,CAAC,MAAM,CAAC,eAAe,CAAC,CAAC,CAAC,CAAC;YAEpC,CAAC,CAAC,kBAAkB,CAAC,CAAC,KAAK,CAAC;gBACxB,CAAC,CAAC,IAAI,CAAC;oBACH,GAAG,EAAE,YAAY;oBACjB,OAAO,EAAE,UAAU,IAAI;wBACnB,IAAI,cAAc,GAAW,OAAO,CAAC,MAAM,CAAC;4BACxC,OAAO,EAAE,IAAI;4BACb,iBAAiB;4BACjB,QAAQ,EAAE,IAAI;4BACd,KAAK,EAAE,mBAAmB;yBAC7B,CAAC,CAAC;wBAEH,cAAc,CAAC,EAAE,CAAC,gBAAgB,EAAE;4BAChC,IAAI,CAAC,mBAAmB,CAAC,cAAc,CAAC,CAAC;4BACzC,IAAI,CAAC,2BAA2B,CAAC,cAAc,EAAE,cAAc,CAAC,CAAC;4BACjE,CAAC,CAAC,WAAW,CAAC,CAAC,KAAK,EAAE,CAAC;wBAC3B,CAAC,CAAC,CAAC;oBACP,CAAC;iBACJ,CAAC,CAAC;YACP,CAAC,CAAC,CAAC;QACP,CAAC;;QAED;;;;;WAKG;QACH,iCAAW,GAAX,UAAY,eAAuB,EAAE,kBAA2B,EAAE,eAAwB;YACtF,CAAC,CAAC,IAAI,CAAC;gBACH,GAAG,EAAE,eAAe;gBACpB,KAAK,EAAE,KAAK;aACf,CAAC,CAAC;YAEH,EAAE,CAAC,CAAC,kBAAkB,CAAC,CAAC,CAAC;gBACrB,IAAI,MAAI,GAAG,IAAI,CAAC;gBAChB,iGAAiG;gBAEjG,2CAA2C;gBAC3C,CAAC,CAAC,IAAI,CAAC;oBACH,GAAG,EAAE,kBAAkB;oBACvB,KAAK,EAAE,KAAK;oBACZ,OAAO,EAAE;wBACL,sDAAsD;wBACtD,QAAQ;wBACR,CAAC,CAAC,IAAI,CAAC;4BACH,QAAQ,EAAE,MAAM;4BAChB,GAAG,EAAE,eAAe;4BACpB,KAAK,EAAE,KAAK;4BACZ,OAAO,EAAE,UAAU,IAAI,EAAE,UAAU,EAAE,KAAK;gCACtC,CAAC,CAAC,IAAI,CAAC,IAAI,EAAE,UAAU,KAAK,EAAE,KAAK;oCAC/B,MAAI,CAAC,iBAAiB,CAAC,KAAK,CAAC,MAAM,CAAC,QAAQ,EAAE,KAAK,CAAC,MAAM,CAAC,EAAE,CAAC,CAAC;gCACnE,CAAC,CAAC,CAAC;4BACP,CAAC;yBACJ,CAAC,CAAC;oBACP,CAAC;iBACJ,CAAC,CAAC;YACP,CAAC;QACL,CAAC;;QAED;;;;;;;;WAQG;QACK,iDAA2B,GAAnC,UAAoC,WAAmB,EAAE,cAAsB;YAC3E,IAAI,YAAY,GAAW,CAAC,CAAC,IAAI,CAAC,wBAAwB,CAAC,gBAAgB,CAAC,CAAC;YAC7E,IAAI,UAAU,GAAW,CAAC,CAAC,IAAI,CAAC,wBAAwB,CAAC,cAAc,CAAC,CAAC;YACzE,IAAI,YAAY,GAAW,CAAC,CAAC,IAAI,CAAC,wBAAwB,CAAC,YAAY,CAAC,CAAC;YAEzE,gEAAgE;YAChE,2CAA2C;YAC3C,EAAE,CAAC,CAAC,YAAY,CAAC,MAAM,CAAC,CAAC,CAAC;gBACtB,IAAI,MAAI,GAAG,IAAI,CAAC;gBAEhB,YAAY,CAAC,KAAK,CAAC,UAAU,KAAwB;oBACjD,KAAK,CAAC,cAAc,EAAE,CAAC;oBAEvB,CAAC,CAAC,IAAI,CAAC;wBACH,GAAG,EAAE,cAAc;wBACnB,IAAI,EAAE,MAAM;wBACZ,IAAI,EAAE;4BACF,EAAE,EAAE,YAAY,CAAC,GAAG,EAAE;4BACtB,IAAI,EAAE,UAAU,CAAC,GAAG,EAAE;4BACtB,SAAS,EAAE,MAAI,CAAC,UAAU;yBAC7B;wBACD,OAAO,EAAE,UAAU,MAAW;4BAC1B,EAAE,CAAC,CAAC,CAAC,MAAM,CAAC,cAAc,CAAC,SAAS,CAAC;gCACjC,CAAC,MAAM,CAAC,cAAc,CAAC,SAAS,CAAC,CAAC,CAAC,CAAC;gCACpC,SAAS,CAAC,MAAM,CAAC,YAAY,CAAC,iCAAiC,GAAG,MAAM,CAAC,CAAC;4BAE9E,CAAC;4BAAC,IAAI,CAAC,EAAE,CAAC,CAAC,MAAM,CAAC,OAAO,KAAK,IAAI,CAAC,CAAC,CAAC;gCACjC,MAAI,CAAC,iBAAiB,CAAC,CAAC,CAAC,iBAAiB,EAAE,YAAY,CAAC,CAAC,IAAI,EAAE,EAAE,YAAY,CAAC,GAAG,EAAE,CAAC,CAAC;gCACtF,WAAW,CAAC,KAAK,CAAC,MAAM,CAAC,CAAC;4BAE9B,CAAC;4BAAC,IAAI,CAAC,CAAC;gCACJ,2CAA2C;gCAC3C,mBAAmB;gCAEnB,IAAI,SAAS,GAA+B,WAAW,CAAC,QAAQ,EAAE,CAAC;gCACnE,SAAS,CAAC,UAAU,CAAC;oCACjB,UAAU,EAAE,MAAM,CAAC,OAAO;iCAC7B,CAAC,CAAA;4BACN,CAAC;wBACL,CAAC;qBACJ,CAAC,CAAC;gBACP,CAAC,CAAC,CAAC;YACP,CAAC;QACL,CAAC;QAED;;;;;;WAMG;QACK,yCAAmB,GAA3B,UAA4B,WAAmB;YAC3C,IAAI,IAAI,GAAG,IAAI,CAAC;YAChB,IAAI,eAAe,GAAW,CAAC,CAAC,MAAM,EAAE,WAAW,CAAC,CAAC;YAErD,CAAC,CAAC,SAAS,CAAC,WAAW,CAAC,KAAK,CAAC,eAAe,CAAC,CAAC;YAE/C,eAAe,CAAC,MAAM,CAAC,UAAU,KAAwB;gBACrD,KAAK,CAAC,cAAc,EAAE,CAAC;gBAEvB,CAAC,CAAC,IAAI,CAAC;oBACH,GAAG,EAAE,IAAI,CAAC,MAAM;oBAChB,IAAI,EAAE,IAAI,CAAC,MAAM;oBACjB,IAAI,EAAE,CAAC,CAAC,IAAI,CAAC,CAAC,SAAS,EAAE;oBACzB,OAAO,EAAE,UAAU,MAAW;wBAC1B,EAAE,CAAC,CAAC,CAAC,MAAM,CAAC,cAAc,CAAC,SAAS,CAAC;4BACjC,CAAC,MAAM,CAAC,cAAc,CAAC,SAAS,CAAC;4BACjC,CAAC,MAAM,CAAC,cAAc,CAAC,IAAI,CAAC,CAAC,CAAC,CAAC;4BAC/B,SAAS,CAAC,MAAM,CAAC,YAAY,CAAC,iCAAiC,GAAG,MAAM,CAAC,CAAC;wBAE9E,CAAC;wBAAC,IAAI,CAAC,EAAE,CAAC,CAAC,MAAM,CAAC,OAAO,KAAK,IAAI,CAAC,CAAC,CAAC;4BACjC,IAAI,CAAC,iBAAiB,CAAC,CAAC,CAAC,WAAW,EAAE,eAAe,CAAC,CAAC,GAAG,EAAE,EAAE,MAAM,CAAC,EAAE,CAAC,CAAC;4BACzE,WAAW,CAAC,KAAK,CAAC,MAAM,CAAC,CAAC;wBAE9B,CAAC;wBAAC,IAAI,CAAC,CAAC;4BACJ,gDAAgD;4BAChD,cAAc;4BAEd,IAAI,SAAS,GAA+B,eAAe,CAAC,QAAQ,EAAE,CAAC;4BACvE,SAAS,CAAC,UAAU,CAAC;gCACjB,UAAU,EAAE,MAAM,CAAC,OAAO;6BAC7B,CAAC,CAAA;wBACN,CAAC;oBACL,CAAC;iBACJ,CAAC,CAAC;gBAEH,MAAM,CAAC,KAAK,CAAC;YACjB,CAAC,CAAC,CAAC;QACP,CAAC;QAED;;;WAGG;QACK,uCAAiB,GAAzB,UAA0B,EAAU;YAChC,IAAI,IAAI,GAAG,IAAI,CAAC;YAEhB,CAAC,CAAC,IAAI,CAAC;gBACH,GAAG,EAAE,IAAI,CAAC,0BAA0B;gBACpC,IAAI,EAAE,QAAQ;gBACd,IAAI,EAAE;oBACF,EAAE,EAAE,EAAE;oBACN,SAAS,EAAE,IAAI,CAAC,UAAU;iBAC7B;gBACD,OAAO,EAAE,UAAU,MAAW;oBAC1B,EAAE,CAAC,CAAC,CAAC,MAAM,CAAC,cAAc,CAAC,SAAS,CAAC;wBACjC,CAAC,MAAM,CAAC,cAAc,CAAC,SAAS,CAAC,CAAC,CAAC,CAAC;wBACpC,SAAS,CAAC,MAAM,CAAC,YAAY,CAAC,iCAAiC,GAAG,MAAM,CAAC,CAAC;oBAE9E,CAAC;oBAAC,IAAI,CAAC,EAAE,CAAC,CAAC,MAAM,CAAC,OAAO,KAAK,IAAI,CAAC,CAAC,CAAC;wBACjC,CAAC,CAAC,eAAe,GAAG,EAAE,CAAC,CAAC,OAAO,CAAC,GAAG,EAAE;4BACjC,CAAC,CAAC,IAAI,CAAC,CAAC,MAAM,EAAE,CAAC;wBACrB,CAAC,CAAC,CAAC;oBAEP,CAAC;oBAAC,IAAI,CAAC,CAAC;wBACJ,SAAS,CAAC,MAAM,CAAC,YAAY,CAAC,MAAM,CAAC,OAAO,CAAC,CAAC;oBAClD,CAAC;gBACL,CAAC;aACJ,CAAC,CAAC;QACP,CAAC;QAED;;;;WAIG;QACK,uCAAiB,GAAzB,UAA0B,QAAgB,EAAE,EAAU;YAClD,IAAI,GAAG,GAAW,CAAC,CAAC,mCAAmC,GAAG,EAAE,GAAG,UAAU,CAAC,CAAC;YAC3E,IAAI,UAAU,GAAW,CAAC,CAAC,yEAAyE,CAAC,CAAC;YACtG,IAAI,YAAoB,CAAC;YACzB,IAAI,IAAI,GAAG,IAAI,CAAC;YAEhB,UAAU,CAAC,MAAM,CAAC,4CAA4C,CAAC,CAAC;YAEhE,UAAU,CAAC,KAAK,CAAC,UAAC,KAAwB;gBACtC,KAAK,CAAC,cAAc,EAAE,CAAC;gBACvB,IAAI,CAAC,iBAAiB,CAAC,EAAE,CAAC,CAAC;YAC/B,CAAC,CAAC,CAAC;YAEH,GAAG,CAAC,MAAM,CAAC,iDAAiD,GAAG,QAAQ,GAAG,cAAc,CAAC,CAAC;YAE1F,YAAY,GAAG,CAAC,CAAC,gCAAgC,CAAC;iBAC7C,MAAM,CAAC,UAAU,CAAC,CAAC;YAExB,CAAC,CAAC,8BAA8B,CAAC;iBAC5B,MAAM,CAAC,YAAY,CAAC;iBACpB,QAAQ,CAAC,GAAG,CAAC,CAAC;YAEnB,GAAG,CAAC,YAAY,CAAC,CAAC,CAAC,WAAW,EAAE,IAAI,CAAC,0BAA0B,CAAC,CAAC,CAAC;QACtE,CAAC;QACL,kBAAC;IAAD,CAAC,AApQD,IAoQC;IApQY,qBAAW,cAoQvB,CAAA;AACL,CAAC,EAtQM,SAAS,KAAT,SAAS,QAsQf"}
+{"version":3,"file":"ActivityLog.js","sourceRoot":"","sources":["ActivityLog.ts"],"names":[],"mappings":"AAAA,IAAO,SAAS,CAoQf;AApQD,WAAO,SAAS,EAAC,CAAC;IACd;QAwBI;;;;;;WAMG;QACH,qBAAY,YAAoB,EAAE,cAAsB,EAAE,iBAAyB,EAAE,SAAiB;YA9BtG,wEAAwE;YAChE,+BAA0B,GAAW,qBAAqB,CAAC;YAE3D,8BAAyB,GAAW,6BAA6B,CAAC;YAE1E,gDAAgD;YACxC,6BAAwB,GAAG;gBAC/B,mDAAmD;gBACnD,gBAAgB,EAAE,uBAAuB;gBAEzC,iDAAiD;gBACjD,cAAc,EAAE,qBAAqB;gBAErC,yDAAyD;gBACzD,YAAY,EAAE,uBAAuB;aACxC,CAAC;YAEF,+DAA+D;YACvD,+BAA0B,GAAW,EAAE,CAAC;YAEhD,2BAA2B;YACnB,eAAU,GAAW,EAAE,CAAC;YAU5B,IAAI,CAAC,0BAA0B,GAAG,iBAAiB,CAAC;YACpD,IAAI,CAAC,UAAU,GAAG,SAAS,CAAC;YAE5B,IAAI,IAAI,GAAG,IAAI,CAAC;YAEhB,SAAS,CAAC,MAAM,CAAC,eAAe,EAAE,CAAC;YACnC,CAAC,CAAC,sBAAsB,CAAC,CAAC,UAAU,EAAE,CAAC;YACvC,SAAS,CAAC,MAAM,CAAC,eAAe,CAAC,CAAC,CAAC,CAAC;YAEpC,CAAC,CAAC,kBAAkB,CAAC,CAAC,KAAK,CAAC;gBACxB,CAAC,CAAC,IAAI,CAAC;oBACH,GAAG,EAAE,YAAY;oBACjB,OAAO,EAAE,UAAU,IAAI;wBACnB,IAAI,cAAc,GAAW,OAAO,CAAC,MAAM,CAAC;4BACxC,OAAO,EAAE,IAAI;4BACb,iBAAiB;4BACjB,QAAQ,EAAE,IAAI;4BACd,KAAK,EAAE,mBAAmB;yBAC7B,CAAC,CAAC;wBAEH,cAAc,CAAC,EAAE,CAAC,gBAAgB,EAAE;4BAChC,IAAI,CAAC,mBAAmB,CAAC,cAAc,CAAC,CAAC;4BACzC,IAAI,CAAC,2BAA2B,CAAC,cAAc,EAAE,cAAc,CAAC,CAAC;4BACjE,CAAC,CAAC,WAAW,CAAC,CAAC,KAAK,EAAE,CAAC;wBAC3B,CAAC,CAAC,CAAC;oBACP,CAAC;iBACJ,CAAC,CAAC;YACP,CAAC,CAAC,CAAC;QACP,CAAC;;QAED;;;;;WAKG;QACI,iCAAW,GAAlB,UAAmB,eAAuB,EAAE,kBAA2B,EAAE,eAAwB;YAC7F,CAAC,CAAC,IAAI,CAAC;gBACH,GAAG,EAAE,eAAe;gBACpB,KAAK,EAAE,KAAK;aACf,CAAC,CAAC;YAEH,EAAE,CAAC,CAAC,kBAAkB,CAAC,CAAC,CAAC;gBACrB,IAAI,MAAI,GAAG,IAAI,CAAC;gBAChB,iGAAiG;gBAEjG,2CAA2C;gBAC3C,CAAC,CAAC,IAAI,CAAC;oBACH,GAAG,EAAE,kBAAkB;oBACvB,KAAK,EAAE,KAAK;oBACZ,OAAO,EAAE;wBACL,sDAAsD;wBACtD,QAAQ;wBACR,CAAC,CAAC,IAAI,CAAC;4BACH,QAAQ,EAAE,MAAM;4BAChB,GAAG,EAAE,eAAe;4BACpB,KAAK,EAAE,KAAK;4BACZ,OAAO,EAAE,UAAU,IAAI;gCACnB,CAAC,CAAC,IAAI,CAAC,IAAI,EAAE,UAAU,KAAK,EAAE,KAAK;oCAC/B,MAAI,CAAC,iBAAiB,CAAC,KAAK,CAAC,MAAM,CAAC,QAAQ,EAAE,KAAK,CAAC,MAAM,CAAC,EAAE,CAAC,CAAC;gCACnE,CAAC,CAAC,CAAC;4BACP,CAAC;yBACJ,CAAC,CAAC;oBACP,CAAC;iBACJ,CAAC,CAAC;YACP,CAAC;QACL,CAAC;;QAED;;;;;;;;WAQG;QACK,iDAA2B,GAAnC,UAAoC,WAAmB,EAAE,cAAsB;YAC3E,IAAI,YAAY,GAAW,CAAC,CAAC,IAAI,CAAC,wBAAwB,CAAC,gBAAgB,CAAC,CAAC;YAC7E,IAAI,UAAU,GAAW,CAAC,CAAC,IAAI,CAAC,wBAAwB,CAAC,cAAc,CAAC,CAAC;YACzE,IAAI,YAAY,GAAW,CAAC,CAAC,IAAI,CAAC,wBAAwB,CAAC,YAAY,CAAC,CAAC;YAEzE,gEAAgE;YAChE,2CAA2C;YAC3C,EAAE,CAAC,CAAC,YAAY,CAAC,MAAM,CAAC,CAAC,CAAC;gBACtB,IAAI,MAAI,GAAG,IAAI,CAAC;gBAEhB,YAAY,CAAC,KAAK,CAAC,UAAU,KAAwB;oBACjD,KAAK,CAAC,cAAc,EAAE,CAAC;oBAEvB,CAAC,CAAC,IAAI,CAAC;wBACH,GAAG,EAAE,cAAc;wBACnB,IAAI,EAAE,MAAM;wBACZ,IAAI,EAAE;4BACF,EAAE,EAAE,YAAY,CAAC,GAAG,EAAE;4BACtB,IAAI,EAAE,UAAU,CAAC,GAAG,EAAE;4BACtB,SAAS,EAAE,MAAI,CAAC,UAAU;yBAC7B;wBACD,OAAO,EAAE,UAAU,MAAW;4BAC1B,EAAE,CAAC,CAAC,CAAC,MAAM,CAAC,cAAc,CAAC,SAAS,CAAC;gCACjC,CAAC,MAAM,CAAC,cAAc,CAAC,SAAS,CAAC,CAAC,CAAC,CAAC;gCACpC,SAAS,CAAC,MAAM,CAAC,YAAY,CAAC,iCAAiC,GAAG,MAAM,CAAC,CAAC;4BAE9E,CAAC;4BAAC,IAAI,CAAC,EAAE,CAAC,CAAC,MAAM,CAAC,OAAO,KAAK,IAAI,CAAC,CAAC,CAAC;gCACjC,MAAI,CAAC,iBAAiB,CAAC,CAAC,CAAC,iBAAiB,EAAE,YAAY,CAAC,CAAC,IAAI,EAAE,EAAE,YAAY,CAAC,GAAG,EAAE,CAAC,CAAC;gCACtF,WAAW,CAAC,KAAK,CAAC,MAAM,CAAC,CAAC;4BAE9B,CAAC;4BAAC,IAAI,CAAC,CAAC;gCACJ,2CAA2C;gCAC3C,mBAAmB;gCAEnB,IAAI,SAAS,GAA+B,WAAW,CAAC,QAAQ,EAAE,CAAC;gCACnE,SAAS,CAAC,UAAU,CAAC;oCACjB,UAAU,EAAE,MAAM,CAAC,OAAO;iCAC7B,CAAC,CAAA;4BACN,CAAC;wBACL,CAAC;qBACJ,CAAC,CAAC;gBACP,CAAC,CAAC,CAAC;YACP,CAAC;QACL,CAAC;QAED;;;;;;WAMG;QACK,yCAAmB,GAA3B,UAA4B,WAAmB;YAC3C,IAAI,IAAI,GAAG,IAAI,CAAC;YAChB,IAAI,eAAe,GAAW,CAAC,CAAC,MAAM,EAAE,WAAW,CAAC,CAAC;YAErD,CAAC,CAAC,SAAS,CAAC,WAAW,CAAC,KAAK,CAAC,eAAe,CAAC,CAAC;YAE/C,eAAe,CAAC,MAAM,CAAC,UAAU,KAAwB;gBACrD,KAAK,CAAC,cAAc,EAAE,CAAC;gBAEvB,CAAC,CAAC,IAAI,CAAC;oBACH,GAAG,EAAE,IAAI,CAAC,MAAM;oBAChB,IAAI,EAAE,IAAI,CAAC,MAAM;oBACjB,IAAI,EAAE,CAAC,CAAC,IAAI,CAAC,CAAC,SAAS,EAAE;oBACzB,OAAO,EAAE,UAAU,MAAW;wBAC1B,EAAE,CAAC,CAAC,CAAC,MAAM,CAAC,cAAc,CAAC,SAAS,CAAC;4BACjC,CAAC,MAAM,CAAC,cAAc,CAAC,SAAS,CAAC;4BACjC,CAAC,MAAM,CAAC,cAAc,CAAC,IAAI,CAAC,CAAC,CAAC,CAAC;4BAC/B,SAAS,CAAC,MAAM,CAAC,YAAY,CAAC,iCAAiC,GAAG,MAAM,CAAC,CAAC;wBAE9E,CAAC;wBAAC,IAAI,CAAC,EAAE,CAAC,CAAC,MAAM,CAAC,OAAO,KAAK,IAAI,CAAC,CAAC,CAAC;4BACjC,IAAI,CAAC,iBAAiB,CAAC,CAAC,CAAC,WAAW,EAAE,eAAe,CAAC,CAAC,GAAG,EAAE,EAAE,MAAM,CAAC,EAAE,CAAC,CAAC;4BACzE,WAAW,CAAC,KAAK,CAAC,MAAM,CAAC,CAAC;wBAE9B,CAAC;wBAAC,IAAI,CAAC,CAAC;4BACJ,gDAAgD;4BAChD,cAAc;4BAEd,IAAI,SAAS,GAA+B,eAAe,CAAC,QAAQ,EAAE,CAAC;4BACvE,SAAS,CAAC,UAAU,CAAC;gCACjB,UAAU,EAAE,MAAM,CAAC,OAAO;6BAC7B,CAAC,CAAA;wBACN,CAAC;oBACL,CAAC;iBACJ,CAAC,CAAC;YACP,CAAC,CAAC,CAAC;QACP,CAAC;QAED;;;WAGG;QACK,uCAAiB,GAAzB,UAA0B,EAAU;YAChC,IAAI,IAAI,GAAG,IAAI,CAAC;YAEhB,CAAC,CAAC,IAAI,CAAC;gBACH,GAAG,EAAE,IAAI,CAAC,0BAA0B;gBACpC,IAAI,EAAE,QAAQ;gBACd,IAAI,EAAE;oBACF,EAAE,EAAE,EAAE;oBACN,SAAS,EAAE,IAAI,CAAC,UAAU;iBAC7B;gBACD,OAAO,EAAE,UAAU,MAAW;oBAC1B,EAAE,CAAC,CAAC,CAAC,MAAM,CAAC,cAAc,CAAC,SAAS,CAAC;wBACjC,CAAC,MAAM,CAAC,cAAc,CAAC,SAAS,CAAC,CAAC,CAAC,CAAC;wBACpC,SAAS,CAAC,MAAM,CAAC,YAAY,CAAC,iCAAiC,GAAG,MAAM,CAAC,CAAC;oBAE9E,CAAC;oBAAC,IAAI,CAAC,EAAE,CAAC,CAAC,MAAM,CAAC,OAAO,KAAK,IAAI,CAAC,CAAC,CAAC;wBACjC,CAAC,CAAC,eAAe,GAAG,EAAE,CAAC,CAAC,OAAO,CAAC,GAAG,EAAE;4BACjC,CAAC,CAAC,IAAI,CAAC,CAAC,MAAM,EAAE,CAAC;wBACrB,CAAC,CAAC,CAAC;oBAEP,CAAC;oBAAC,IAAI,CAAC,CAAC;wBACJ,SAAS,CAAC,MAAM,CAAC,YAAY,CAAC,MAAM,CAAC,OAAO,CAAC,CAAC;oBAClD,CAAC;gBACL,CAAC;aACJ,CAAC,CAAC;QACP,CAAC;QAED;;;;WAIG;QACK,uCAAiB,GAAzB,UAA0B,QAAgB,EAAE,EAAU;YAClD,IAAI,GAAG,GAAW,CAAC,CAAC,mCAAmC,GAAG,EAAE,GAAG,UAAU,CAAC,CAAC;YAC3E,IAAI,UAAU,GAAW,CAAC,CAAC,yEAAyE,CAAC,CAAC;YACtG,IAAI,YAAoB,CAAC;YACzB,IAAI,IAAI,GAAG,IAAI,CAAC;YAEhB,UAAU,CAAC,MAAM,CAAC,4CAA4C,CAAC,CAAC;YAEhE,UAAU,CAAC,KAAK,CAAC,UAAC,KAAwB;gBACtC,KAAK,CAAC,cAAc,EAAE,CAAC;gBACvB,IAAI,CAAC,iBAAiB,CAAC,EAAE,CAAC,CAAC;YAC/B,CAAC,CAAC,CAAC;YAEH,GAAG,CAAC,MAAM,CAAC,iDAAiD,GAAG,QAAQ,GAAG,cAAc,CAAC,CAAC;YAE1F,YAAY,GAAG,CAAC,CAAC,gCAAgC,CAAC;iBAC7C,MAAM,CAAC,UAAU,CAAC,CAAC;YAExB,CAAC,CAAC,8BAA8B,CAAC;iBAC5B,MAAM,CAAC,YAAY,CAAC;iBACpB,QAAQ,CAAC,GAAG,CAAC,CAAC;YAEnB,GAAG,CAAC,YAAY,CAAC,CAAC,CAAC,WAAW,EAAE,IAAI,CAAC,0BAA0B,CAAC,CAAC,CAAC;QACtE,CAAC;QACL,kBAAC;IAAD,CAAC,AAlQD,IAkQC;IAlQY,qBAAW,cAkQvB,CAAA;AACL,CAAC,EApQM,SAAS,KAAT,SAAS,QAoQf"}

src/Belletrix.Web/Scripts/App/ActivityLog.ts
@@ -194,8 +194,6 @@
                         }
                     }
                 });
-
-                return false;
             });
         }
 