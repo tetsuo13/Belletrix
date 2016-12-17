c7a8cd5514691e2af05cb5b4d4530e3afeab55d3
Andrei Nicholson <contact@andreinicholson.com>
2016-12-16 21:49:08 -0500

    Complete schema

--- Schema/schema.sql
+++ Schema/schema.sql
@@ -733,13 +733,17 @@ CREATE TABLE [dbo].[Documents] (
     [CreatedBy]         [int] NOT NULL,
     [LastModified]      [datetime],
     [LastModifiedBy]    [int],
+    [Deleted]           [datetime],
+    [DeletedBy]         [int],
     [ActivityLogId]     [int],
     [Title]             [nvarchar](1024) NOT NULL,
     [Size]              [int] NOT NULL,
+    [MimeType]          [varchar](128) NOT NULL,
     [Content]           [varbinary](max) NOT NULL,
 
     CONSTRAINT [FK_Documents_CreatedBy] FOREIGN KEY ([CreatedBy]) REFERENCES [dbo].[Users] ([Id]),
-    CONSTRAINT [FK_Documents_ActivityLogId] FOREIGN KEY ([ActivityLogId]) REFERENCES [dbo].[ActivityLog] ([Id])
+    CONSTRAINT [FK_Documents_ActivityLogId] FOREIGN KEY ([ActivityLogId]) REFERENCES [dbo].[ActivityLog] ([Id]),
+    CONSTRAINT [FK_Documents_DeletedBy] FOREIGN KEY ([DeletedBy]) REFERENCES [dbo].[Users] ([Id])
 );
 
 
